6ce568004919fa906108ac51b367efc14aadd5eb
pranaykumarsamala <samalapranay02@gmail.com>
2025-04-27 19:11:36 +0530

    addes styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Child1Component } from "./child1/child1.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Child1Component, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Password Generator';
+  rangeValue: number=0;
+  numbersValue: boolean=false;
+  charactersValue: boolean=false;
+
 }
---
+++ src/app/child1/child1.component.ts
@@ -0,0 +1,45 @@
+import { formatCurrency } from '@angular/common';
+import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-child1',
+  imports: [FormsModule],
+  templateUrl: './child1.component.html',
+  styleUrl: './child1.component.css'
+})
+export class Child1Component implements OnChanges{
+  @Input() passwordLength: number=0;
+  @Input() numbersAllowed: boolean=false;
+  @Input() charactersAllowed: boolean=false;
+
+  @ViewChild('displayPass') ref!: ElementRef;
+  pass = "";
+  displayText='';
+
+  copyPassword(){
+    this.displayText=this.ref.nativeElement.textContent;
+    navigator.clipboard.writeText(this.displayText);
+
+  }
+  generatePassword(){
+    this.pass="";
+    let dataString='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if(this.numbersAllowed==true){
+      dataString+='0123456789';
+    }
+  
+    if(this.charactersAllowed==true){
+      dataString+='!@#$%^&*()_+';
+    }
+
+    for(let i = 0; i < this.passwordLength; i++) {
+      const char = Math.floor(Math.random() * dataString.length)
+      this.pass += dataString.charAt(char)
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      this.generatePassword();
+  }
+}
